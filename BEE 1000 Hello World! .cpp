7c776da0989bb044179b64ae09fb91e2ad08e960
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-27 22:24:22 -0800

    Add files via upload

---
+++ BEE 1000 Hello World! .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    cout<<"Hello World!"<<endl;
+    return 0 ;
+}
