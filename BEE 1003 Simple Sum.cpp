744c9fde60015c4b302b2c5e7a7a44ce3e725709
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2023-12-30 13:53:01 +0600

    Add files via upload

---
+++ BEE 1003 Simple Sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+
+    int a,b;
+    cin>>a>>b;
+    cout<<"SOMA = "<<a+b<<endl;
+
+    return 0;
+}
