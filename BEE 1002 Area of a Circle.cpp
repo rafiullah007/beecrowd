cf656ded6ce03173babbfd9b671cd1817f9bd45b
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-04 18:32:24 +0600

    Add files via upload

---
+++ BEE 1002 Area of a Circle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    /*
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+    */
+
+int main() {
+    double r, A;
+    const double pi = 3.14159;
+    cin >> r;
+    A = pi * r * r;
+    cout << fixed << setprecision(4);
+    cout << "A=" << A << endl;
+
+    return 0;
+}
