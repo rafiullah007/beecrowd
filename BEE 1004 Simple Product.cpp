411d845031a798278672d3b5d11352dfe35a29f8
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-07 23:13:28 +0600

    Add files via upload

---
+++ BEE 1004 Simple Product.cpp
@@ -0,0 +1,22 @@
+
+#include<iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int A, B, PROD;
+    cin >> A >> B;
+    PROD = A * B;
+    cout << "PROD = " << PROD << endl;
+    return 0;
+}
