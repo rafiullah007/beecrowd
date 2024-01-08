4064368d3f7b17fee93bf7db8238163ae35863c0
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-08 22:22:57 +0600

    Add files via upload

---
+++ BEE 1001 Extremely Basic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+int sum(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<"X = "<<sum(a,b)<<endl;
+}
