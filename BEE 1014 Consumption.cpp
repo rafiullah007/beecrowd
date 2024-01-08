a88bc3365b5975fba55a5d2d5e7f757d5d98d9c6
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-08 22:15:49 +0600

    Add files via upload

---
+++ BEE 1014 Consumption.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int a;
+    double b;
+    cin>>a;
+    cin>>b;
+    double sum = a/b;
+    cout << fixed << setprecision(3) << sum << " km/l" << endl;
+    return 0;
+}
