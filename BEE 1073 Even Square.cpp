ad0802317ae79d1dc2a54eab426157717e05e9d5
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-12 22:12:17 +0600

    Add files via upload

---
+++ BEE 1073 Even Square.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+    /**
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main() {
+    int s;
+    cin>>s;
+    for(int i=2;i<=s;i+=2)
+    {
+        int sum = i*i;
+        cout<<i<<"^2 ="<<" "<<sum<<endl;
+    }
+
+}
+
