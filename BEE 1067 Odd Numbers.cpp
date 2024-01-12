25549fbc11a49c7e820962eaaae83f83cecc6112
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-12 21:54:07 +0600

    Add files via upload

---
+++ BEE 1067 Odd Numbers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+    /**
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    for(int i = 1;i<=n;i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<endl;
+        }
+    }
+    return 0;
+}
+
