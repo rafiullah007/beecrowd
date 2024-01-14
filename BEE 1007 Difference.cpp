42e8796724e665db6f3373698f1d538c4ab4e086
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-14 23:40:38 +0600

    Add files via upload

---
+++ BEE 1007 Difference.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+    int sum = (a*b)-(c*d);
+    cout <<"DIFERENCA ="<<" "<<sum<<endl;
+    return 0;
+}
